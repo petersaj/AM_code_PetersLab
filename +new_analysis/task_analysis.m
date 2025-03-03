94128eeb3796077e6b59efeab0281b4d2026e487
andrada08 <andrada.maria.marica@gmail.com>
2025-03-03 16:24:43 +0000

    plot per day MUAs
    - fixed flattening (permute first)
    - removed old code
    - currently MUA plot sorted by resp

--- +new_analysis/task_analysis.m
+++ +new_analysis/task_analysis.m
@@ -51,71 +51,63 @@ num_recordings = height(task_ephys);
 % unique_stims = unique_stims_nan(~isnan(unique_stims_nan));
 
 %% - make big vectors of days from learning and mouse id
-
-%% !!!!!!!!!!! LEFT HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+% maps_n_depths = arrayfun(@(rec_idx) ...
+%     size(all_ctx_maps_to_str.cortex_kernel_px{rec_idx}, 3) * ~isempty(all_ctx_maps_to_str.cortex_kernel_px{rec_idx}), ...
+%     1:height(all_ctx_maps_to_str));
+% 
+% find(maps_n_depths - n_depths)
 
 n_depths = arrayfun(@(rec_idx) ...
     size(task_ephys.binned_spikes_stim_align{rec_idx}, 3) * ~isempty(task_ephys.binned_spikes_stim_align{rec_idx}), ...
     1:num_recordings);
-
-maps_n_depths = arrayfun(@(rec_idx) ...
-    size(all_ctx_maps_to_str.cortex_kernel_px{rec_idx}, 3) * ~isempty(all_ctx_maps_to_str.cortex_kernel_px{rec_idx}), ...
-    1:height(all_ctx_maps_to_str));
-
-find(maps_n_depths - n_depths)
-
-num_trials = arrayfun(@(rec_idx) ...
-    size(task_ephys.binned_spikes_stim_align{rec_idx}, 1) * ~isempty(task_ephys.binned_spikes_stim_align{rec_idx}), ...
-    1:num_recordings);
+all_psth_depth_cell = arrayfun(@(rec_idx) 1:n_depths(rec_idx), 1:num_recordings, 'uni', false);
 
 per_rec_cluster_ids = mat2cell(cluster_ids, n_depths);
 
+% move from depth to cluster
+per_rec_psth_cluster_ids = cell(num_recordings, 1);
+for rec_idx=1:num_recordings
+    cluster = per_rec_cluster_ids{rec_idx};
+    psth_depth = all_psth_depth_cell{rec_idx};
+    this_psth_cluster_ids = zeros(size(psth_depth));
+    this_psth_cluster_ids(~isnan(psth_depth)) = cluster(psth_depth(~isnan(psth_depth)));
+    per_rec_psth_cluster_ids{rec_idx} = this_psth_cluster_ids;
+end
+% all_psth_cluster_ids = horzcat(psth_cluster_ids{:});
 
-for_psth_cluster_ids_cell = arrayfun(@(rec_idx) repelem(1:num_trials(rec_idx), n_depths(rec_idx))', ...
+% put cluster ids into num_trials
+num_trials = arrayfun(@(rec_idx) ...
+    size(task_ephys.binned_spikes_stim_align{rec_idx}, 1) * ~isempty(task_ephys.binned_spikes_stim_align{rec_idx}), ...
+    1:num_recordings);
+for_psth_cluster_ids_cell = arrayfun(@(rec_idx) repelem(per_rec_psth_cluster_ids{rec_idx}, num_trials(rec_idx))', ...
     1:num_recordings, 'uni', false);
-for_psth_cluster_ids_all = vertcat(for_psth_cluster_ids_cell{:});
+for_psth_cluster_ids = vertcat(for_psth_cluster_ids_cell{:});
 
-for_psth_n_depths = repelem(n_depths, num_trials);
-size(for_psth_n_depths)
-sum(n_depths.*num_trials)
-
-expected_size = sum(num_trials)
-
-% Repeat trial indices for each depth
-trial_idx_cell = arrayfun(@(rec_idx) repelem(1:num_trials(rec_idx), n_depths(rec_idx))', ...
+% get trial ids
+for_psth_trial_ids_cell = arrayfun(@(rec_idx) repelem(1:num_trials(rec_idx), n_depths(rec_idx))', ...
     1:num_recordings, 'uni', false);
-trial_idx_all = vertcat(trial_idx_cell{:});
+for_psth_trial_ids = vertcat(for_psth_trial_ids_cell{:});
 
-% Repeat depth indices for each trial
-depth_idx_cell = arrayfun(@(rec_idx) repelem(1:n_depths(rec_idx), num_trials(rec_idx))', ...
+% disp(length(for_psth_cluster_ids_all))
+% disp(sum(n_depths.*num_trials))
+
+% get days from learning and animal id
+for_psth_days_from_learning = repelem(bhv.days_from_learning, n_depths.*num_trials);
+[~, ~, for_psth_animal_ids] = unique(repelem(bhv.animal, n_depths.*num_trials));
+
+% get flattened psths
+% permute first
+permute_stim_binned_spikes = arrayfun(@(rec_idx) ...
+    permute(task_ephys.binned_spikes_stim_align{rec_idx}, [1 3 2]), ...
     1:num_recordings, 'uni', false);
-depth_idx_all = vertcat(depth_idx_cell{:});
-
-% Combine the trial and depth indices into a single matrix
-trial_depth_index = [trial_idx_all, depth_idx_all];
-
-% for_psth_days_from_learning = repelem(bhv.days_from_learning, n_depths);
-% [~, ~, for_psth_animal_ids] = unique(repelem(bhv.animal, n_depths));
-
-
-% try to get psths 
-test_transp = task_ephys.binned_spikes_stim_align.';
-test_non_empty = test_transp(~cellfun(@isempty, test_transp)); % Keeps only non-empty cells
-
-all_stim_binned_spikes = cat(1, test_non_empty{:});
-
-
 flattened_stim_binned_spikes = arrayfun(@(rec_idx) ...
-    reshape(task_ephys.binned_spikes_stim_align{rec_idx}, ...
+    reshape(permute_stim_binned_spikes{rec_idx}, ...
     n_depths(rec_idx)*num_trials(rec_idx), ...
-    size(task_ephys.binned_spikes_stim_align{rec_idx}, 2)), ...
+    size(permute_stim_binned_spikes{rec_idx}, 3)), ...
     1:num_recordings, 'uni', false);
-
 cat_flattened_stim_binned_spikes = cat(1, flattened_stim_binned_spikes{:});
 
-
-
-A_back = reshape(flattened_stim_binned_spikes{2}, num_trials(2), 1501, n_depths(2));
+% A_back = reshape(flattened_stim_binned_spikes{2}, num_trials(2), 1501, n_depths(2));
 
 % figure;
 % imagesc(task_ephys.binned_spikes_stim_align{2}(:,:,1))
@@ -123,98 +115,95 @@ A_back = reshape(flattened_stim_binned_spikes{2}, num_trials(2), 1501, n_depths(
 % figure;
 % imagesc(A_back(:,:,1))
 
-% %% PASSIVE STUFF
-% %% - get psths
-% 
-% all_avg_stim_grouped_binned_spikes = cell(numel(unique_stims), 1);
-% for stim_idx = 1:numel(unique_stims)
-%     stim_grouped_binned_spikes = arrayfun(@(rec_idx) ...
-%         task_ephys.binned_spikes_stim_align{rec_idx}(ephys.trial_stim_values{rec_idx} == unique_stims(stim_idx), :, :), ...
-%         1:num_recordings, 'UniformOutput', false);
-%     avg_stim_grouped_binned_spikes = cellfun(@(rec_spikes) ...
-%         squeeze(mean(rec_spikes, 1)), ...
-%         stim_grouped_binned_spikes, 'UniformOutput', false);
-%     all_avg_stim_grouped_binned_spikes {stim_idx} = horzcat(avg_stim_grouped_binned_spikes{:});
-% end
 
 %% - group psths according to animal, cluster and day from learning
 % only use nonnan learning days
 use_days = ~isnan(for_psth_days_from_learning);
 
-[unique_cluster_ids, ~, ~] = unique(cluster_ids(use_days));
+[unique_cluster_ids, ~, ~] = unique(for_psth_cluster_ids(use_days));
 [unique_days_from_learning, ~, ~] = unique(for_psth_days_from_learning(use_days));
 [unique_animal_ids, ~, ~] = unique(for_psth_animal_ids(use_days));
 
 % Combine cluster_ids and days into a single matrix for indexing
-group_indices = [for_psth_animal_ids(use_days), cluster_ids(use_days), for_psth_days_from_learning(use_days)];
+group_indices = [for_psth_trial_ids(use_days), for_psth_animal_ids(use_days), for_psth_cluster_ids(use_days), for_psth_days_from_learning(use_days)];
 [group_indices_unique_clusters, ~, group_clusters_indices] = unique(group_indices, 'rows');
 
-all_sum_avg_stim_grouped_spikes = cell(numel(unique_stims), 1);
-for stim_idx = 1:numel(unique_stims)
-    all_sum_avg_stim_grouped_spikes{stim_idx} = ap.groupfun(@sum, ...
-        all_avg_stim_grouped_binned_spikes{stim_idx}(:, use_days), [], group_clusters_indices);
+grouped_cluster_stim_psths = ap.groupfun(@sum, ...
+        cat_flattened_stim_binned_spikes(use_days, :), group_clusters_indices, []);
+
+
+%% norm and smooth
+time_vector = -0.5:0.001:1;
+baseline_idx = time_vector >= -0.2 & time_vector <= 0;
+
+% Function to normalize and smooth
+gauss_win = gausswin(51, 3)';
+normalize_and_smooth = @(psth) ...
+    filter(gauss_win, sum(gauss_win), (psth - mean(psth(baseline_idx), 2)) ./ mean(psth(baseline_idx), 2), [], 2);
+
+grouped_cluster_smooth_norm_stim_psths = cell2mat(arrayfun(@(rep) ...
+    normalize_and_smooth(grouped_cluster_stim_psths(rep, :)), ...
+    1:size(grouped_cluster_stim_psths, 1), ...
+    'UniformOutput', false)');
+
+%% package in cluster and day
+
+norm_smooth_stim_psths = cell(num_clusters, length(unique_days_from_learning));
+for cluster_id=1:num_clusters
+    for day_idx=1:length(unique_days_from_learning)
+        norm_smooth_stim_psths{cluster_id, day_idx} = ...
+            grouped_cluster_smooth_norm_stim_psths(...
+            group_indices_unique_clusters(:, 3) == cluster_id & ...
+            group_indices_unique_clusters(:, 4) == unique_days_from_learning(day_idx), :);
+    end
 end
 
-% group num trials
-grouped_num_trials = cell(numel(unique_stims), 1);
-for stim_idx=1:numel(unique_stims)
-    grouped_num_trials{stim_idx} = accumarray(group_clusters_indices, for_psth_num_trials{stim_idx}(use_days)', ...
-        [length(unique(group_clusters_indices)), 1], @unique);
+
+%% plot sorted by resp
+sort_time = time_vector > 0 & time_vector < 0.2;
+sorted_norm_smooth_stim_psths = cell(num_clusters, length(unique_days_from_learning));
+for cluster_id=1:num_clusters
+    for day_idx=1:length(unique_days_from_learning)
+        [~, sort_idx] = sort(mean(norm_smooth_stim_psths{cluster_id, day_idx}(:, sort_time), 2), 'descend');
+        sorted_norm_smooth_stim_psths{cluster_id, day_idx} = norm_smooth_stim_psths{cluster_id, day_idx}(sort_idx, :);
+    end
 end
 
+plot_time = [-0.5 1];
+plot_time_idx = time_vector > plot_time(1) & time_vector < plot_time(2);
+for cluster_id=1:num_clusters
+    figure;
+    tiledlayout('flow')
+    for day_idx=1:length(unique_days_from_learning)
+        nexttile;
+        imagesc(time_vector(plot_time_idx), [], sorted_norm_smooth_stim_psths{cluster_id, day_idx}(:, plot_time_idx))
+        max_abs = max(abs(sorted_norm_smooth_stim_psths{cluster_id, day_idx}(:, plot_time_idx)), [], 'all');
+        if isempty(max_abs)
+            continue
+        end
+        clim([-max_abs max_abs])
+        colormap(ap.colormap('BWR'))
+        title(['Day ' num2str(unique_days_from_learning(day_idx))])
+    end
+    sgtitle(['Cluster ' num2str(cluster_id)])
+end
 
 % test = group_indices_unique_clusters(:,[1,3]);
 % size(unique(test, 'rows'))
 % 
 % sum(cellfun(@(x) ~isempty(x), ephys.trial_stim_values) & ~isnan(bhv.days_from_learning))
 
+%% make RT sorting
+all_RT = vertcat(bhv.stim_to_move);
 
-%% - get baseline
-time_vector = -0.5:0.001:1;
-baseline_idx = time_vector >= -0.2 & time_vector <= 0;
+% empty the ones we don't use
+all_RT(num_trials == 0) = [];
 
-per_stim_baseline_avg = cellfun(@(x) mean(x(baseline_idx, :), 1), all_sum_avg_stim_grouped_spikes, 'UniformOutput', false);
-total_num_trials = cell2mat(grouped_num_trials')';
-weighted_sum_stim_baseline = cell2mat(cellfun(@(per_stim_baseline, num_trials) per_stim_baseline .* num_trials', ...
-    per_stim_baseline_avg, grouped_num_trials, 'UniformOutput', false));
-weighted_avg_stim_baseline = sum(weighted_sum_stim_baseline, 1) ./ sum(total_num_trials, 1);
-
-%% - norm smooth psths
-gauss_win = gausswin(51, 3)';
-normalize_and_smooth = @(psth, rep) ...
-    filter(gauss_win, sum(gauss_win), (psth - weighted_avg_stim_baseline(rep)) ./ (weighted_avg_stim_baseline(rep)), [], 1);
-
-all_smooth_stim_psths = cell(numel(unique_stims), 1);
-for stim_idx=1:numel(unique_stims)
-    all_smooth_stim_psths{stim_idx} = cell2mat(arrayfun(@(rep) ...
-        normalize_and_smooth(squeeze(all_sum_avg_stim_grouped_spikes{stim_idx}(:, rep)), rep), ...
-        1:size(all_sum_avg_stim_grouped_spikes{stim_idx}, 2), ...
-        'UniformOutput', false));
-end
+vertcat(all_RT)
 
 
-%% - average psths across mice
+for_psth_trial_ids_cell = arrayfun(@(rec_idx) repelem(1:num_trials(rec_idx), n_depths(rec_idx))', ...
+    1:num_recordings, 'uni', false);
+for_psth_trial_ids = vertcat(for_psth_trial_ids_cell{:});
 
-% mouse idx is first
-avg_animal_group_indices = group_indices_unique_clusters(:,2:3);
-[unique_avg_animal_group_indices, ~, animal_group_clusters_indices] = unique(avg_animal_group_indices, 'rows');
 
-avg_smooth_stim_psths = cell(numel(unique_stims), 1);
-for stim_idx=1:numel(unique_stims)
-    avg_smooth_stim_psths{stim_idx} = ap.groupfun(@mean, ...
-        all_smooth_stim_psths{stim_idx}, [], animal_group_clusters_indices);
-end
-
-num_animals_stim_psths = cell(numel(unique_stims), 1);
-for stim_idx = 1:numel(unique_stims)
-    num_animals_stim_psths{stim_idx} = accumarray(animal_group_clusters_indices, 1);
-end
-
-% do sem for errorbar
-std_smooth_stim_psths = cell(numel(unique_stims), 1);
-sem_smooth_stim_psths = cell(numel(unique_stims), 1);
-for stim_idx=1:numel(unique_stims)
-    std_smooth_stim_psths{stim_idx} = ap.groupfun(@std, ...
-        all_smooth_stim_psths{stim_idx}, [], animal_group_clusters_indices);
-    sem_smooth_stim_psths{stim_idx} = std_smooth_stim_psths{stim_idx} ./ sqrt(num_animals_stim_psths{stim_idx})';
-end

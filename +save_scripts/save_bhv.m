2817fe106e1cc5cf0ee61bbf75c7a555e2fb53fd
Andy Peters <peters.andrew.j@gmail.com>
2025-05-30 16:35:39 +0100

    Update save_bhv.m

--- +save_scripts/save_bhv.m
+++ +save_scripts/save_bhv.m
@@ -1,3 +1,5 @@
+%%%%%% AP MODIFIED: ADDED LEARNING P-VALUES FROM OTHER STATS
+
 save_path = '\\qnap-ap001.dpag.ox.ac.uk\APlab\Users\Andrada-Maria_Marica\long_str_ctx_data';
 
 animals = { ...
@@ -23,13 +25,26 @@ for animal_idx=1:length(animals)
         verbose = true;
         load_parts.behavior = true;
         ap.load_recording
-
-        [stimwheel_pval,...
-            stimwheel_rxn_mean, ...
-            stimwheel_rxn_null_mean] = ...
+    
+        % Get association p-value in a few ways
+        % (mean to firstmove)
+        [stimwheel_pval_firstmove_mean,stimwheel_rxn_firstmove_mean,stimwheel_rxn_null_firstmove_mean] = ...
             AP_stimwheel_association_pvalue(stimOn_times,trial_events,stim_to_move, 'mean');
 
-        learned_days(use_rec) = stimwheel_pval < 0.05;
+        % (mad to firstmove)
+        [stimwheel_pval_firstmove_mad,stimwheel_rxn_firstmove_mad,stimwheel_rxn_null_firstmove_mad] = ...
+            AP_stimwheel_association_pvalue(stimOn_times,trial_events,stim_to_move, 'mad');
+
+        % (mean to lastmove)
+        [stimwheel_pval_lastmove_mean,stimwheel_rxn_lastmove_mean,stimwheel_rxn_null_lastmove_mean] = ...
+            AP_stimwheel_association_pvalue(stimOn_times,trial_events,stim_to_lastmove, 'mean');
+
+        % (mad to lastmove)
+        [stimwheel_pval_lastmove_mad,stimwheel_rxn_lastmove_mad,stimwheel_rxn_null_lastmove_mad] = ...
+            AP_stimwheel_association_pvalue(stimOn_times,trial_events,stim_to_lastmove, 'mad');
+
+        % Define learned day
+        learned_days(use_rec) = stimwheel_pval_firstmove_mean < 0.05;
 
         if use_rec==1
             learned_days(use_rec) = 0;
@@ -42,13 +57,27 @@ for animal_idx=1:length(animals)
         % save
         bhv_animal.animal(use_rec) = {animal};
         bhv_animal.rec_day(use_rec) = {rec_day};
-        bhv_animal.stimwheel_pval(use_rec) = {stimwheel_pval};
-        bhv_animal.stimwheel_rxn_mean(use_rec) = {stimwheel_rxn_mean};
-        bhv_animal.stimwheel_rxn_null_mean(use_rec) = {stimwheel_rxn_null_mean};
+        % (firstmove mean stats)
+        bhv_animal.stimwheel_pval_firstmove_mean(use_rec) = {stimwheel_pval_firstmove_mean};
+        bhv_animal.stimwheel_rxn_firstmove_mean(use_rec) = {stimwheel_rxn_firstmove_mean};
+        bhv_animal.stimwheel_rxn_null_firstmove_mean(use_rec) = {stimwheel_rxn_null_firstmove_mean};
+        % (firstmove mad stats)
+        bhv_animal.stimwheel_pval_firstmove_mad(use_rec) = {stimwheel_pval_firstmove_mad};
+        bhv_animal.stimwheel_rxn_firstmove_mad(use_rec) = {stimwheel_rxn_firstmove_mad};
+        bhv_animal.stimwheel_rxn_null_firstmove_mad(use_rec) = {stimwheel_rxn_null_firstmove_mad};
+        % (lastmove mean stats)
+        bhv_animal.stimwheel_pval_lastmove_mean(use_rec) = {stimwheel_pval_lastmove_mean};
+        bhv_animal.stimwheel_rxn_lastmove_mean(use_rec) = {stimwheel_rxn_lastmove_mean};
+        bhv_animal.stimwheel_rxn_null_lastmove_mean(use_rec) = {stimwheel_rxn_null_lastmove_mean};
+        % (lastmove mad stats)
+        bhv_animal.stimwheel_pval_lastmove_mad(use_rec) = {stimwheel_pval_lastmove_mad};
+        bhv_animal.stimwheel_rxn_lastmove_mad(use_rec) = {stimwheel_rxn_lastmove_mad};
+        bhv_animal.stimwheel_rxn_null_lastmove_mad(use_rec) = {stimwheel_rxn_null_lastmove_mad};
+
         % reaction times
         bhv_animal.stim_to_move(use_rec) = {stim_to_move(1:n_trials)};
         bhv_animal.trial_outcome(use_rec) = {trial_outcome(1:n_trials)};
-        bhv_animal.stim_to_reward(use_rec) = {stim_to_reward(1:n_trials)};
+        bhv_animal.stim_to_reward(use_rec) = {stim_to_reward};
 
     end
 
@@ -65,16 +94,16 @@ end
 
 bhv = vertcat(all_bhv_cell{:});
 
-figure;
-for animal_idx=1:length(animals)
-    animal = animals(animal_idx);
-    this_stim_wheel_pval = cell2mat(bhv.stimwheel_pval(strcmp(bhv.animal, animal)));
-    plot(this_stim_wheel_pval);
-    hold on
-end
-legend(animals)
+% figure;
+% for animal_idx=1:length(animals)
+%     animal = animals(animal_idx);
+%     this_stim_wheel_pval = cell2mat(bhv.stimwheel_pval(strcmp(bhv.animal, animal)));
+%     plot(this_stim_wheel_pval);
+%     hold on
+% end
+% legend(animals)
 
-save_name = fullfile(save_path, 'swr_bhv');
+save_name = fullfile(save_path, 'swr_bhv_v2');
 save(save_name, "bhv", "-v7.3");
 
 clear all;
